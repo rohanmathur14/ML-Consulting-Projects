20723b3a4d948ff9b35e66724217066b007395a4
rijushm <rijushm@gmail.com>
2021-07-30 20:50:40 +0530

    new

--- nbatopshot/assets/js/script.js
+++ nbatopshot/assets/js/script.js
@@ -60,4 +60,28 @@ var swiper = new Swiper(".home-section-2-mob", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+    });
+
+ var swiper = new Swiper(".open-openings-slide", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 8,
+            spaceBetween: 0,
+          },
+        },
       });
